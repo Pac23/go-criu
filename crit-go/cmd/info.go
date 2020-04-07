d88420ccd2828dd9d0c21581795167ec160d086d
Manas Mangaonkar <manasmangaonkar@gmail.com>
2020-04-07 23:15:55 +0530

    refactored info functionality file with stding support for blank --in flag

--- crit-go/cmd/info.go
+++ crit-go/cmd/info.go
@@ -1,8 +1,11 @@
 package cmd
 
 import (
-	"fmt"
-
+	//"fmt"
+	"bufio"
+	"crit-go/gocrit"
+	"os"
+	//"errors"
 	"github.com/spf13/cobra"
 )
 
@@ -10,27 +13,31 @@ import (
 var infoCmd = &cobra.Command{
 	Use:   "info",
 	Short: "Gives info of the image",
-	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) error {
-    	if len(args) < 1 {
-      		return errors.New("requires a -i argument with image")
-    	}
-		Info(args []string)
-	}
+	Long: `Shows detailed information about the image
+	 such as its magic no`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if inloc == "" {
+			reader := bufio.NewReader(os.Stdin)
+			stdininp, err := reader.ReadString('\n')
+			gocrit.Check(err)
+			/*
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+			*/
+			gocrit.Info(stdininp)
+			/*
+				if err != nil {
+					fmt.Println(err)
+				}
+			*/
+			gocrit.Check(err)
+		} else {
+			gocrit.Info(inloc)
+		}
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(infoCmd)
-	infoCmd.Flags().String("in", "i", "", 'show info about criu image (stdin by default)')
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// infoCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// infoCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	infoCmd.Flags().StringVarP(&inloc, "in", "i", "", "show info about criu image (stdin by default)")
 }
-
