ee73ccffe4d523357cd3f1ad220bbbbc3baf9d15
Manas Mangaonkar <manasmangaonkar@gmail.com>
2020-04-07 22:51:54 +0530

    re factored basic structure of decode with added check for stdin

--- crit-go/cmd/decode.go
+++ crit-go/cmd/decode.go
@@ -1,40 +1,36 @@
 package cmd
 
 import (
-	"fmt"
+	"bufio"
+	"crit-go/gocrit"
+	//"errors"
+	//"fmt"
 
 	"github.com/spf13/cobra"
+	"os"
 )
 
 // decodeCmd represents the decode command
 var decodeCmd = &cobra.Command{
 	Use:   "decode",
 	Short: "Convert Binary to Json",
-	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) error {
-    	if len(args) < 1 {
-      		return errors.New("requires a -i argument with image")
-    	}
-		Decode(args []string)
-	}
+	Long: `Converts the Binary file to Json and writes to given location
+	pretty printing of json is available aswell using the pretty flag for
+	more info visit https://www.criu.org/CRIT#Functionality `,
+	Run: func(cmd *cobra.Command, args []string) {
+		if inloc == "" {
+			reader := bufio.NewReader(os.Stdin)
+			stdininp, err := reader.ReadString('\n')
+			gocrit.Check(err)
+			gocrit.Decode(stdininp, outloc, pretty)
+		} else {
+			gocrit.Decode(inloc, outloc, pretty)
+		}
+	},
 }
-
 
 func init() {
-	rootCmd.AddCommand(decodeCmd)
-	decodeCmd.Flags().String("in", "i", "", 'criu image in binary format to be decoded (stdin by default)')
-	decodeCmd.Flags().Bool("pretty", "p", false, 'Multiline with indents and some numerical fields in field-specific format')
-
-
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// decodeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// decodeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	decodeCmd.Flags().StringVarP(&inloc, "in", "i", "", "criu image in binary format to be decoded (stdin by default)")
+	decodeCmd.Flags().StringVarP(&outloc, "out", "o", "", "where to put the image in json format(Stdout by default)")
+	decodeCmd.Flags().BoolVarP(&pretty, "pretty", "p", false, "Multiline with indents and some numerical fields in field-specific format")
 }
-
