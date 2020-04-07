0d7238865205e787e877de38a5958cd95763a0f3
Manas Mangaonkar <manasmangaonkar@gmail.com>
2020-04-07 23:17:13 +0530

    refactored show functionality file with stding input

--- crit-go/cmd/show.go
+++ crit-go/cmd/show.go
@@ -1,34 +1,31 @@
 package cmd
 
 import (
-	"fmt"
-
+	//"fmt"
+	"bufio"
+	"crit-go/gocrit"
 	"github.com/spf13/cobra"
+	"os"
+	//"errors"
 )
 
 // showCmd represents the show command
 var showCmd = &cobra.Command{
 	Use:   "show",
 	Short: "Converts Binary to Json and Displays full output",
-	Long: ``,
-	Run: func(cmd *cobra.Command, args []string) error {
-    	if len(args) < 2 {
-      		return errors.New("requires atleast one argument")
-    	}
-		Show(args []string)
-	}
+	Long:  ``,
+	Run: func(cmd *cobra.Command, args []string) {
+		if inloc == "" {
+			reader := bufio.NewReader(os.Stdin)
+			stdininp, err := reader.ReadString('\n')
+			gocrit.Check(err)
+			gocrit.Show(stdininp)
+		} else {
+			gocrit.Show(inloc)
+		}
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(showCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// showCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// showCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	showCmd.Flags().StringVarP(&inloc, "in", "i", "", "criu image in binary format to be decoded (stdin by default)")
 }
